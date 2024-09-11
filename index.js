5e184d9eb1d649ae5a98ce2f3fa0fdd52f848fac
kevin fajardo <kevinfajardo1103@gmail.com>
2024-09-11 18:40:58 -0400

    Changes to the website, added index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalid = null;
+
+function initialzeSlider() {
+
+}
+function showSlide(index) {
+
+    slides[slideIndex].class
+
+}
+function prevSlide() {
+
+}
+function nextSlide() {
+
+}
